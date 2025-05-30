e9d462a04ec4032c851d51e6dd37e3e69bbbf833
MarcoVLGE <188342088+MarcoFormatica@users.noreply.github.com>
2025-05-30 16:08:07 +0200

    Update GameManager.cs

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -20,24 +20,24 @@ public class GameManager : NetworkBehaviour
             for (int i = 0; i < 30; i++)
             {
                 Debug.Log("i");
-                SpawnCube(new Vector3(i,0,j), EblockType.Erba,1);
+                SpawnCube(new Vector3(i,0,j), (int)EblockType.Erba,1);
                 yield return new WaitForSeconds(0.1f);
             }
         }
     }
 
     [Rpc(sources:RpcSources.All, targets:RpcTargets.StateAuthority)]
-    public void RPC_SpawnCube(Vector3 position, EblockType type, int indestructable)
+    public void RPC_SpawnCube(Vector3 position, int type, int indestructable)
     {
         Debug.Log(nameof(RPC_SpawnCube));
         SpawnCube(position, type, indestructable);
     }
 
-    private void SpawnCube(Vector3 position, EblockType type, int indestructable)
+    private void SpawnCube(Vector3 position, int type, int indestructable)
     {
         Debug.Log(nameof(SpawnCube) + position.ToString());
         NetworkObject spawnedBlockGO = Runner.Spawn(blockPrefab, position);
-        spawnedBlockGO.GetComponent<Block>().InitializeBlock(type);
+        spawnedBlockGO.GetComponent<Block>().InitializeBlock((EblockType)type);
         spawnedBlockGO.GetComponent<Block>().Indestructible = indestructable;
         spawnedBlockGO.transform.position = position;
     }
